ecc9c9153dc8cc14038c6ebbfcc17aa3cabf6a21
Bob Khounsombath <bob.khounsombath@gmail.com>
2024-01-27 20:19:39 -0800

    navbar component setup

---
+++ static/javascript/components/navbar.js
@@ -0,0 +1,58 @@
+import { createApp } from 'vue'; // Using module system setup
+
+const app = createApp({ // Using module system setup
+// const app = Vue.createApp({ // using vue.global.js
+    template: `
+        <ul>
+            <li v-for= "(hrefLink, page, index) in pages" :key="page">
+                <a :href="hrefLink" target="_self">
+                    {{ page }}
+                </a>
+            </li>
+        </ul>
+    `,
+
+    data() {
+        return {
+            pages: {
+                'Foyer': 'index.html',
+                'About': 'static/html/about.html', 
+                'Contact': 'static/html/contact.html'
+            }
+
+        }
+    }
+})
+// app.config.globalProperties.$myGlobalVariable = 'value'
+
+
+/* dynamic nav construction
+(function injectNavigationList(targetID){
+    const pages={
+        'Foyer': 'index.html',
+        'About': 'about.html', 
+        'Contact': 'contact.html'
+    };
+
+    // const fragment = document.createDocumentFragment();
+    const ul = document.createElement('ul');
+
+    Object.keys(pages).forEach(page=>{
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.setAttribute('href', pages[page]);
+        a.setAttribute('target', '_blank');
+        // a.setAttribute('rel', 'noreferrer noopener nofollow');
+        a.textContent = page;
+        li.appendChild(a);
+        ul.appendChild(li);
+    });
+    // fragment.appendChild(ul);
+    // document.getElementById(targetID).appendChild(fragment);
+    document.getElementById(targetID).appendChild(ul);
+})//('navbar');
+*/
+
+
+// app.mount('#navbar')
+export default app; // Using module system setup
