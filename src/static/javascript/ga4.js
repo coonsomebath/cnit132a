6e3bee4f52debd94a75c7ee08f5ef4f3b7166be9
Bob Khounsombath <bob.khounsombath@gmail.com>
2024-02-11 18:22:16 -0800

    attempted to sub img for video to decrease load time

--- src/static/javascript/ga4.js
+++ src/static/javascript/ga4.js
@@ -6,3 +6,19 @@ funnelSourceElement.onchange = function(e) {
     }
     document.getElementById('funnel-iframe').setAttribute('src', source[e.target.value])
 }
+
+/* Attempt to decrease load time; lack of autoplay makes for poor UX.
+const thumbnailImg = document.getElementById('ga4-thumbnail');
+thumbnailImg.onclick = function(e){
+    const iframeGA4IntroElem = document.createElement('iframe');
+    iframeGA4IntroElem.title = "Introduction to Google Analytics 4";
+    iframeGA4IntroElem.width = "560";
+    iframeGA4IntroElem.height = "315";
+    iframeGA4IntroElem.src = "https://www.youtube.com/embed/u_ECkoHVlZ8?si=cdLxYuBk6bsgCI6q";
+    iframeGA4IntroElem.frameborder = "0";
+    // iframeGA4IntroElem.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
+    iframeGA4IntroElem.allowfullscreen = true;
+
+    e.target.replaceWith(iframeGA4IntroElem);
+}
+*/
