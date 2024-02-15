6a55df320b00e2ca98744f94a91539c3063acccc
Bob Khounsombath <bob.khounsombath@gmail.com>
2024-02-15 15:31:15 -0800

    removed dirty dev code related to sidebar and layout debugging

--- src/static/javascript/components/sidebar.js
+++ src/static/javascript/components/sidebar.js
@@ -8,45 +8,6 @@ window.addEventListener('load', ()=> {
 
         isExpanded = isExpanded === 'true' ? 'false' : 'true';
         navbar.setAttribute('aria-expanded', isExpanded);
-        // navbar.style.opacity = isExpanded==="true" ? 1 : 1;
-
-
-        // (function setHamburgerOffset(element){
-        //     /* piggy backing onclick to set variable */
-        //     const html = document.documentElement;
-        //     const isOverflow = html.scrollHeight > html.clientHeight;
-
-        //     if(isOverflow){
-        //         // has scrollbar
-        //         console.log('has scrollbar');
-        //         navbar.style.setProperty('--right-padding', '1rem');
-        //     }
-        //     else{
-        //         // no scrollbar
-        //         console.log('no scrollbar');
-        //         navbar.style.setProperty('--right-padding', '2rem');
-        //     }
-        // })(navbar)
-
-        // (function setHamburgerOffset(element){
-        //     /* piggy backing onclick to set variable */
-        //     const html = document.documentElement;
-        //     const isOverflow = html.scrollHeight > html.clientHeight;
-        //     // const hasWedge = document.querySelector('.scrollbar-wedge');
-
-        //     if(hasWedge){
-        //         // navbar.style.setProperty('--wedge-padding', '2rem');
-        //         // no scrollbar
-        //         console.log('no scrollbar');
-        //         navbar.style.setProperty('--right-padding', '2rem');
-        //     }
-        //     else{
-        //         // navbar.style.removeProperty('--wedge-padding');
-        //         // has scrollbar
-        //         console.log('has scrollbar');
-        //         navbar.style.setProperty('--right-padding', '1rem');
-        //     }
-        // })(navbar)
     }
 })
 
