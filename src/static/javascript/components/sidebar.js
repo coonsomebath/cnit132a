5fc59c5dc8c8173a3cc2225e13eb09099562716e
Bob Khounsombath <bob.khounsombath@gmail.com>
2024-02-15 10:48:56 -0800

    stabilized layout & hamburger; garbage code not deleted yet

--- src/static/javascript/components/sidebar.js
+++ src/static/javascript/components/sidebar.js
@@ -15,8 +15,16 @@ window.addEventListener('load', ()=> {
             /* piggy backing onclick to set variable */
             const hasWedge = document.querySelector('.scrollbar-wedge');
             if(hasWedge){
-
                 navbar.style.setProperty('--wedge-padding', '2rem');
+                // no scrollbar
+                console.log('no scrollbar');
+                navbar.style.setProperty('--right-padding', '2rem');
+            }
+            else{
+                navbar.style.removeProperty('--wedge-padding');
+                // has scrollbar
+                console.log('has scrollbar');
+                navbar.style.setProperty('--right-padding', '1rem');
             }
         })(navbar)
     }
